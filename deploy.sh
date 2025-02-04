9faca24b1f8d8053ec5fbe8e3d74c12283835e38
gedemais <geoffrey.demaiso@gmail.com>
2025-02-04 12:11:55 +0100

    uploads added

--- deploy.sh
+++ deploy.sh
@@ -36,13 +36,13 @@ bash deploy_mysql.sh
 
 # Copie du site web vulnérable dans le répertoire d'Apache
 sudo cp -r ../vulnerable_website/* /var/www/html/
+mkdir /var/www/html/uploads
 
 # Modification des droits d'accès pour les fichiers du site
 sudo chown -R www-data:www-data /var/www/html/
 sudo chmod -R 755 /var/www/html/
 
 # Ouverture des ports HTTP et HTTPS dans le pare-feu
-sudo ufw up
 sudo ufw allow 80/tcp
 sudo ufw allow 443/tcp
 sudo ufw reload
